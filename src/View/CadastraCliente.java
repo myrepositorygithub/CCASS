39ee3c04374e8bc57de74ba7b7c6518512cf9301
Unknown <thiagoo.cabral@gmail.com>
2017-06-29 01:24:39 -0400

    Spellcheck cadastro
    Verificação dos campos de entrada para registro de novo associado

--- src/View/CadastraCliente.java
+++ src/View/CadastraCliente.java
@@ -2,10 +2,14 @@ package View;
 
 import Control.Associado;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  * Created by thiago on 6/28/17.
@@ -34,24 +38,106 @@ public class CadastraCliente {
     private Button addDependent;
 
     @FXML
-    private boolean novoAssociado(){
+    private void novoAssociado() {
 
-        if(nameField.getText().isEmpty()){
+        if (nameField.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Campo Obrigatório");
+            alert.setHeaderText(null);
+            alert.setContentText("Insira o nome completo!\n");
+            alert.showAndWait();
             System.out.println("Campo Obrigatório: Nome");
-        }if(rgField.getText().isEmpty()){
-            System.out.println("Campo Obrigatório: RG");
-        }if(cpfField.getText().isEmpty()){
-            System.out.println("Campo Obrigatório: CPF");
-        }if(addrField.getText().isEmpty()){
-            System.out.println("Campo Obrigatório: Endereço");
-        }if(foneField.getText().isEmpty()){
-            System.out.println("Campo Obrigatório: Telefone");
-        }
-        Associado novo = new Associado(nameField.getText(),cpfField.getText(),rgField.getText()+"-"+rgField2.getText(),
-                addrField.getText(), new ArrayList<String>());
+        } else if (!rgField.getText().matches("[0-9]{8}")) {
+            if (rgField.getText().isEmpty()) {
 
-        System.out.println("tentando cadastrar novo Associado: "+nameField.getText());
-        return true;
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Campo Obrigatório");
+                alert.setHeaderText(null);
+                alert.setContentText("Insira os 8 digitos do RG!\nEx: 12345678");
+                alert.showAndWait();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("RG inválido");
+                alert.setHeaderText(null);
+                alert.setContentText("Insira apenas os 8 digitos!\nEx: 12345678");
+                alert.showAndWait();
+            }
+            System.out.println("Campo Obrigatório: RG");
+        } else if (!cpfField.getText().matches("[0-9]{11}")) {
+            if (cpfField.getText().isEmpty()) {
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Campo Obrigatório");
+                alert.setHeaderText(null);
+                alert.setContentText("Insira os 11 digitos do CPF!\nEx: 12345678911");
+                alert.showAndWait();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("CPF inválido");
+                alert.setHeaderText(null);
+                alert.setContentText("Insira apenas os 11 digitos.\nEx: 12345678911");
+                alert.showAndWait();
+            }
+            System.out.println("Campo Obrigatório: CPF");
+        } else if (addrField.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Campo Obrigatório");
+            alert.setHeaderText(null);
+            alert.setContentText("Insira o Endereço!\n");
+
+            alert.showAndWait();
+            System.out.println("Campo Obrigatório: Endereço");
+        } else if (foneField.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Campo Obrigatório");
+            alert.setHeaderText(null);
+            alert.setContentText("Insira o Telefone de contato!\n");
+
+            alert.showAndWait();
+            System.out.println("Campo Obrigatório: Telefone");
+        } else if (!foneField.getText().matches("[0-9]{10}") && !foneField.getText().matches("[0-9]{11}")) {
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("RG inválido");
+            alert.setHeaderText(null);
+            alert.setContentText("Insira o Telefone com o DDD(Apenas numeros)!\nEx: 6599564016");
+            alert.showAndWait();
+        } else {
+            String rg = rgField.getText().substring(0, 7) + "-" + rgField.getText().charAt(7);
+
+            String cpf = cpfField.getText().substring(0, 3) + "." + cpfField.getText().substring(3, 6) + "." +
+                    cpfField.getText().substring(6, 9) + "-" + cpfField.getText().substring(9, 11);
+            String phone = "("+foneField.getText().substring(0,2) +")"+ foneField.getText().substring(2);
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmação");
+            alert.setHeaderText("Detalhes do associado");
+            alert.setContentText("Nome: \t\t" + nameField.getText() + "\n" +
+                    "RG: \t\t\t" + rg + " " + rgField2.getText() + "\n" +
+                    "CPF: \t\t\t" + cpf + "\n" +
+                    "Endereço: \t" + addrField.getText() + "\n" +
+                    "Telefone: \t\t" + phone + "\n" +
+                    "Dependentes: \t" + "\n");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                // ... user chose OK
+                Associado novo = new Associado(nameField.getText(), cpfField.getText(), rgField.getText() + "-" + rgField2.getText(),
+                        addrField.getText(), new ArrayList<String>());
+
+                System.out.println("tentando cadastrar novo Associado: " + nameField.getText());
+                closeButtonAction();
+            }
+        }
+    }
+
+
+    @FXML
+    private void closeButtonAction() {
+        // get a handle to the stage
+        Stage stage = (Stage) cancela.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 
 }
--- src/View/Controller.java
+++ src/View/Controller.java
@@ -8,7 +8,8 @@ import javafx.scene.control.MenuItem;
 import javafx.event.ActionEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import java.io.IOException;
+
+import java.io.*;
 
 public class Controller {
 
@@ -66,6 +67,58 @@ public class Controller {
 
         System.out.println("testa impressao");
     }
+
+    public void printCheck(ActionEvent actionEvent) {
+
+        System.out.println("testa impressao");
+        try {
+            FileOutputStream os = new FileOutputStream("LPT1");
+            //wrap stream in "friendly" PrintStream
+            PrintStream ps = new PrintStream(os);
+
+            //print text here
+            ps.println("Can anybody find me somebody to love?\n" +
+                    "Each morning I get up I die a little\n" +
+                    "Can barely stand on my feet\n" +
+                    "(Take a look at yourself) Take a look in the mirror and cry\n" +
+                    "Lord what you're doing to me\n" +
+                    "I have spent all my years in believing you\n" +
+                    "But I just can't get no relief Lord\n" +
+                    "Somebody (somebody) ooh somebody (somebody)\n" +
+                    "Can anybody find me somebody to love?\n" +
+                    "\n" +
+                    "I work hard (he works hard) everyday of my life\n" +
+                    "I work till I ache my bones\n" +
+                    "At the end (at the end of the day)\n" +
+                    "I take home my hard earned pay all on my own\n" +
+                    "I get down (down) on my knees (knees)\n" +
+                    "And I start to pray (praise the Lord)\n" +
+                    "Till the tears run down from my eyes\n" +
+                    "Lord somebody (somebody) ooh somebody (please)\n" +
+                    "Can anybody find me somebody to love?\n" +
+                    "(He wants help)\n" +
+                    "\n" +
+                    "Every day - I try and I try and I try\n" +
+                    "But everybody wants to put me down\n" +
+                    "They say I'm goin' crazy\n" +
+                    "They say I got a lot of water in my brain\n" +
+                    "Got no common sense\n" +
+                    "I got nobody left to believe\n" +
+                    "Yeah - yeah yeah yeah\n" +
+                    "Ooh\n" +
+                    "\n");
+
+            //form feed -- this is important
+            //Without the form feed, the text will simply sit
+            // in print buffer until something else gets printed.
+            ps.print("\f");
+            //flush buffer and close
+            ps.close();
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e);
+        }
+    }
+
 
     public void cadastraAssociado(ActionEvent actionEvent) {
 
